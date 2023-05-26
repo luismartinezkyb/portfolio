fbe32dc1cff7e9fcefa615ecb9d7239f8b6de16b
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-05-25 23:00:18 -0600

    certificaciones

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -9,7 +9,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { useContext } from "react";
 import { LanguageContext } from "../context/LanguageContext";
 
-
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 
 const ProjectCard = ({
   index,
@@ -27,13 +27,13 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl w-[300px] sm:w-[280px] lg:w-[300px]  justify-center items-center lg:h-[250px] sm:h-[270px]"
       >
-        <div className="relative w-full h-[230px]">
+        <div className="relative w-[250px] justify-center items-center flex">
           <img
             src={image}
             alt={name}
-            className="w-full h-full object-cover rounded-2xl"
+            className="w-[150px] object-cover rounded-2xl"
           />
         </div>
         <div className="absolute inset-0 justify-end card-img_hover flex m-3">
@@ -41,25 +41,24 @@ const ProjectCard = ({
             onClick={() => window.open(public_url, "_blank")}
             className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
           >
-            <img
-              src={github}
-              alt="github"
-              className="w-1/2 h-1/2 object-contain"
-            />
+            <BsFillArrowUpRightCircleFill></BsFillArrowUpRightCircleFill>
           </div>
         </div>
 
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        <div className="mt-5 items-center flex justify-center">
+          <h3 className="text-white font-bold text-[15px]">{name}</h3>
+          {/* <div className='max-h-[200px] overflow-y-auto'>
+            
+          </div>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p> */}
         </div>
-        <div className="mt-4 flex flex-wrap gap-2">
+        {/* <div className="mt-4 flex flex-wrap gap-2 opacity-60">
           {tags.map((tag, index) => (
             <p key={index} className={`text-[14px] ${tag.color}`}>
               #{tag.name}
             </p>
           ))}
-        </div>
+        </div> */}
       </Tilt>
     </motion.div>
   );
